88bf746cce70fad12758e0708dd2b66843d4240a
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-10-21 10:46:42 -0400

    Updated logic in getPitchedScale.

--- src/Notes.js
+++ src/Notes.js
@@ -1,22 +1,10 @@
-export const notes = ['c', 'c#', 'd', 'd#', 'e', 'e#', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
-
+export const notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
 export const scales = {
   major: [0, 2, 4, 5, 7, 9 , 11],
-  minor: [0, 2, 3, 5, 7, 8 , 11],
+  minor: [0, 2, 3, 5, 7, 8 , 10],
   pentatonic: [0, 2, 4, 7, 9],
   chromatic: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 
   diminished: [0, 1, 3, 4, 6, 7, 9, 10]
-}
-
-export const duration = {
-  "1/4": "0:0:1",
-  "2/4": "0:0:2",
-  "3/4": "0:0:3",
-  "4/4": "0:1:0",
-  "6/4": "0:1:2",
-  "8/4": "0:2:0",
-  "10/4":"0:2:2",
-  "12/4":"0:3"
 }
 
 /** Convert a midi note signal to a pitch frequency. */
@@ -25,11 +13,53 @@ export function midiToFrequency(m) {
   return freq;
 }
 
+/** Given a set of degrees or notes, returns as a new list whose contents are randomized from list. */
+export function randomFromList(list, length) {
+  const notes = [];
+  for (let i = 0; i < length; i++) 
+    notes.push(list[randomInt(0, randomNumber.length)]);
+
+  return restifyList(notes);
+}
+
+export function restifyList(notes) {
+  console.warn("This is only a stub. ", restifyLists);
+  return notes;
+}
+
+/** Convert a pattern of scale degrees to target key center 
+  * formatted for use with Tidal.js */
+export function getPitchedScale(mode = ['d', 'minor'], octave = 4) {
+  const [ key, scale ] = mode;
+  const fundamental = notes.findIndex((root) => root == key.toLowerCase());
+  return scales[scale].map((degree, index, degrees) => {
+    let location = fundamental + degree;
+    let mod = 0;
+    if (location >= notes.length) {
+      location -= notes.length;
+      mod++;
+    }
+    const note = notes[location];
+    return note.toUpperCase() + (octave + mod);
+  })
+}
+
 /** Convert a scale degree to a frequency. 
  * Notes are provided between 0 and 11 with an octave modifer. */
 export function degreeToMidi(note, octave = 5, root = 'c') {
   const keyMod = notes.findIndex((pitch) => pitch == root.toLowerCase());
   return keyMod + note + (octave * 12);
+}
+
+export function degreeToPitches(pattern, mode = ['d', 'minor'], octave = 4) {
+  const [ key, scale ] = mode;
+  const fundamental = notes.findIndex((root) => root == key.toLowerCase());
+  const target = scales[scale];
+  return pattern.reduce((pitches, degree, index) => {
+    const note = notes[(fundamental + degree) % notes.length];
+    pitches.push(note.toUpperCase() + octave);
+    return pitches;
+  }, [])
 }
 
 /** Create a valid Time Event as [timestamp, freq]. */
@@ -58,8 +88,28 @@ export function diatonicHarmony(pattern, scale = 'major', voices = 3) {
   }, []);
 }
 
+/** Harmonize a pattern by a constant interval. 
+  * Useful for stacking perfect 4ths, 5ths, and 8ves. */
+export function intervallicHarmony(pattern, interval = 4, voices = 3) {
+  return pattern.reduce((harmonies, degree, index, src) => {
+    if (!harmonies.length) 
+      for (let i = 0; i < voices; i++) harmonies[i] = [];
+    
+    for (let i = 0; i < voices; i++) {
+      const rootIndex = scales.chromatic.findIndex((d) => d == pattern[i]);
+      const harmDegree = (rootIndex + interval) % scales.chromatic.length;
+      harmonies[i].push(harmDegree);
+    }
+
+    return harmonies;
+  }, []);
+}
+
 /** Augment or diminish a time pattern by a constant scale. */
 export function scaleTime(timeList, scale = 2) {
+  if (typeof timeList == 'string')
+    timeList = timestampToList(timeList);
+
   console.log("Preparing scaled time : ", timeList.map(t => t * scale));
   return normalizeTimeList(timeList.map(t => t * scale));
 }
@@ -69,6 +119,7 @@ export function timeListToString(timeList) {
   return timeList.join(':');
 }
 
+/** Formats a provided timeList into a correct beat divisions. */
 function normalizeTimeList(timeList) {
   const limit = 4;
   const newList = timeList.reduce((newList, part, index, src) => {
@@ -108,23 +159,6 @@ function normalizeTimeList(timeList) {
   return newList;
 }
 
-/** Harmonize a pattern by a constant interval. 
-  * Useful for stacking perfect 4ths, 5ths, and 8ves. */
-export function intervallicHarmony(pattern, interval = 4, voices = 3) {
-  return pattern.reduce((harmonies, degree, index, src) => {
-    if (!harmonies.length) 
-      for (let i = 0; i < voices; i++) harmonies[i] = [];
-    
-    for (let i = 0; i < voices; i++) {
-      const rootIndex = scales.chromatic.findIndex((d) => d == pattern[i]);
-      const harmDegree = (rootIndex + interval) % scales.chromatic.length;
-      harmonies[i].push(harmDegree);
-    }
-
-    return harmonies;
-  }, []);
-}
-
 /** Transform a timestring to a numeric list of [bars, quarters, sixteenths]. */
 export function timestampToList(time) {
   if (time.indexOf(':') == -1)
@@ -137,4 +171,8 @@ export function timestampToList(time) {
 export function parseInput(pitchString) {
   return pitchString.split(/,|\s+/)
     .map(n => n.length ? parseInt(n) : n); // allow empty strings for empty beats.
+}
+
+function randomInt(min= 1, max = 10) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
